ca5660b3af017faeaa4d2a85481ec4de6c94f91a
Zach Wingrave <github@zachwingrave.com>
2024-12-23 09:50:18 +1100

    Update import statements to be explicit

--- src/gui/mainApplicationWindow.py
+++ src/gui/mainApplicationWindow.py
@@ -1,37 +1,39 @@
-import PySide6 as qt
-import PySide6.QtCore as qtc
-import PySide6.QtWidgets as qtw
-import PySide6.QtGui as qtg
+import PySide6
+import PySide6.QtCore
+import PySide6.QtWidgets
+import PySide6.QtGui
 import sys
 import random
 
 # Prints PySide6 version and Qt version used to compile PySide6
-print("PySide6:", qt.__version__)
-print("PySide6.QTCore:", qtc.__version__)
+print("PySide6:", PySide6.__version__)
+print("PySide6.QTCore:", PySide6.QtCore.__version__)
 
 
-class MyWidget(qtw.QWidget):
+class MyWidget(PySide6.QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
         self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
 
-        self.button = qtw.QPushButton("Click me!")
-        self.text = qtw.QLabel("Hello World", alignment=qtc.Qt.AlignCenter)
+        self.button = PySide6.QtWidgets.QPushButton("Click me!")
+        self.text = PySide6.QtWidgets.QLabel(
+            "Hello World", alignment=PySide6.QtCore.Qt.AlignCenter
+        )
 
-        self.layout = qtw.QVBoxLayout(self)
+        self.layout = PySide6.QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
 
         self.button.clicked.connect(self.magic)
 
-    @qtc.Slot()
+    @PySide6.QtCore.Slot()
     def magic(self):
         self.text.setText(random.choice(self.hello))
 
 
 if __name__ == "__main__":
-    app = qtw.QApplication([])
+    app = PySide6.QtWidgets.QApplication([])
 
     widget = MyWidget()
     widget.resize(400, 400)
